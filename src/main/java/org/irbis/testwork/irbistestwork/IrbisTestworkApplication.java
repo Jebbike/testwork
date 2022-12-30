99e63bd782f90623bcd004514539a1ed1db25cdb
Yaroslav Ryabov <rya@irbis.plus>
2022-12-30 15:32:07 +0400

    init project

---
+++ src/main/java/org/irbis/testwork/irbistestwork/IrbisTestworkApplication.java
@@ -0,0 +1,13 @@
+package org.irbis.testwork.irbistestwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrbisTestworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrbisTestworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/irbis/testwork/irbistestwork/IrbisTestworkApplicationTests.java
@@ -0,0 +1,13 @@
+package org.irbis.testwork.irbistestwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrbisTestworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
